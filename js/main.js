17aa1fac62bb9619e1a4c20cdf8c0530abfb981c
Nick Wills <37917347+KingOfCaves@users.noreply.github.com>
2018-11-28 11:15:42 -0500

    fixing a lot of styling and layout issues,  adding in the api key from mapbox, adding responsive design

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoia2luZ29mY2F2ZXMiLCJhIjoiY2pvdmxoOGJnMGFkZzNxbzN3dTdleWFoayJ9.kdVFk9hmsRIp4H-8vska_g',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -208,4 +208,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
-
